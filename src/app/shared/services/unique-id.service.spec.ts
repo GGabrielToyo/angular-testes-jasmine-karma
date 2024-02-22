a02e8d59cb904bc324b90bf590fe2203df451dc9
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2024-02-22 14:48:51 -0300

    Iniciando com testes no Angular.

---
+++ src/app/shared/services/unique-id.service.spec.ts
@@ -0,0 +1,12 @@
+import { UniqueIdService } from "./unique-id.service";
+
+describe('UniqueIdService', () =>{
+
+    it('#generateUniqueIdWithPrefix should generate id when called with prefix', () =>{
+        const service = new UniqueIdService();
+        const id = service.generateUniqueIdWithPrefix('app');
+
+        expect(id).toContain('app-');
+    });
+
+});
